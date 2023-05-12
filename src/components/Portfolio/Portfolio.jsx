cc727a6310a80afbee5601ec47699eb32ac7af9d
ilia <iliajuso@gmail.com>
2023-05-12 19:51:20 +0400

    add Footer

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import s from "./Portolio.sass"
+import s from "./Portolio.module.sass"
+import Woman from "./PorfolioImg/Слой_8.jpg"
+import Levi from "./PorfolioImg/reebok_web.png"
+import Camera from "./PorfolioImg/reebok_web.jpg"
 export const Portfolio = () => {
     return (
       <section className={s.select}>
-            <h2>Portfolio</h2>
-            <div className={s.container}>
+           <div className={s.container}>
     <h2>Portfolio</h2>
-    <img className={s.main_portfolio_levi} src={"./PorfolioImg/Слой_8.jpg"} alt="Portfolio"/>
+    <img className={s.main_portfolio_levi} src={Woman} alt="Portfolio"/>
     <a href="https://www.levi.com/US/en_US/"><p>Online fashion store - Homepage</p></a>
-    <img className={s.main_portfolio_pos} src={"./PorfolioImg/reebok_web.png"} alt="Reebok"/>
+    <img className={s.main_portfolio_pos} src={Levi} alt="Reebok"/>
     <a href="https://www.reebok.com/us"><p>Reebok Store - Concept</p></a>
-    <img className={s.main_portfolio_pos} src={"./PorfolioImg/reebok_web.jpg"} alt="Braun"/>
+    <img className={s.main_portfolio_pos} src={Camera} alt="Braun"/>
     <a href="https://www.braunhousehold.com/en"><p>Braun Landing Page - Concept</p></a>
   </div>
     </section>

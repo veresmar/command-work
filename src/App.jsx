6b5405f477be3c83c8d01354fc326a8589a2de04
veresmar <veres.maria.dev@gmail.com>
2023-05-12 20:41:26 +0400

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
 import { Skills } from './components/Skills/Skills';
 import { AboutMe } from './components/AboutMe/AboutMe';
+import { Header } from './components/Header/Header';
+import { Portfolio } from './components/Portfolio/Portfolio';
+import { Footer } from './components/Footer/Footer';
+import { MainScreen } from './components/MainScreen/MainScreen';
 
 function App() {
   return (
     <div className="App">
-      <h1>There will be a team project</h1>
-      <Skills></Skills>
+      <Header/>
+
       <AboutMe></AboutMe>
+      <Skills></Skills>
+      <Portfolio></Portfolio>
+      <Footer></Footer>
     </div>
   );
 }
